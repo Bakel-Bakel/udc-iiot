d677d0277cbd3a6ab63c63ba4cf6d0c1008c95ce
bakel-bakel <begededum4bakel@gmail.com>
2025-09-17 11:12:03 +0200

    flag

--- exercise-1/ex1.py
+++ exercise-1/ex1.py
@@ -6,15 +6,16 @@ sense = SenseHat()
 R = [255, 0, 0]
 W = [255, 255, 255]
 B = [0, 0, 0]
+G = [0,255,0]
 
 question = [
-W, W, W, R, R, W, W, W,
-W, W, R, W, W, R, W, W,
-W, W, W, W, W, R, W, W,
-W, W, W, W, R, W, W, W,
-W, W, W, R, W, W, W, W,
-W, W, W, R, W, W, W, W,
-W, W, W, W, W, W, W, W,
-W, W, W, R, W, W, W, W
+G, G, G, W, W, G, G, G,
+G, G, G, W, W, G, G, G,
+G, G, G, W, W, G, G, G,
+G, G, G, W, W, G, G, G,
+G, G, G, W, W, G, G, G,
+G, G, G, W, W, G, G, G,
+G, G, G, W, W, G, G, G,
+G, G, G, W, W, G, G, G
 ]
 sense.set_pixels(question)
