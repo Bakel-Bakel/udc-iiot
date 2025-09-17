03c57ceb1d14c069f5bfa332641e4fed607a5753
bakel-bakel <begededum4bakel@gmail.com>
2025-09-17 11:06:52 +0200

    RWB

---
+++ exercise-1/ex1.py
@@ -0,0 +1,19 @@
+from sense_hat import SenseHat
+from time import sleep, time
+
+sense = SenseHat()
+
+R = [255, 0, 0]
+W = [255, 255, 255]
+B = [0, 0, 0]
+
+question = [
+W, W, W, R, R, W, W, W,
+W, W, R, W, W, R, W, W,
+W, W, W, W, W, R, W, W,
+W, W, W, W, R, W, W, W,
+W, W, W, R, W, W, W, W,
+W, W, W, R, W, W, W, W,
+W, W, W, W, W, W, W, W,
+W, W, W, R, W, W, W, W
+]
